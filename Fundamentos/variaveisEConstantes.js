dbca62221ea27e3faa85f5ec7a55028a115a154c
AristotelesPantaleao <62292967+AristotelesPantaleao@users.noreply.github.com>
2020-09-28 17:57:24 -0300

    Create variaveisEConstantes.js

---
+++ Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,28 @@
+// O Básico de var, let e const
+
+// Variáveis e Constantes
+
+// var nome da variável recebe o valor 3
+var a = 3
+
+// let nome da variável recebe o valor 4
+let b = 4
+
+// Crie variáveis usando let
+
+var a = 30
+b = 40
+
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+// const nome da variável recebe o valor 5
+const c = 5
+
+// Por ser uma constante o valor não pode ser mudado
+// c = 50 (daria um erro)
+console.log(c)
