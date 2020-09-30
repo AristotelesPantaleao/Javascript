eaa24c2c079d8c50b612fa9fb7a928df4a3cf4ce
AristotelesPantaleao <62292967+AristotelesPantaleao@users.noreply.github.com>
2020-09-30 18:10:46 -0300

    Create numeros.js

---
+++ Fundamentos/numeros.js
@@ -0,0 +1,39 @@
+// Tipos em JavaScript: Number.
+
+// Formas de declarar números.
+
+const peso1 = 1.0
+const peso2 = Number('2.0')
+
+console.log(peso1, peso2)
+
+// Alguns métodos dentro da função Number.
+
+// Saber se o valor de peso1 é inteiro.
+console.log(Number.isInteger(peso1))
+
+// Saber se o valor de peso2 é inteiro.
+console.log(Number.isInteger(peso2))
+
+// Algumas operações
+
+const avaliacao1 = 9.871
+const avaliacao2 = 6.871
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2
+
+const media = total / ( peso1 + peso2 )
+
+console.log(media)
+
+// O método toFixed() formata um número utilizando notação de ponto fixo.
+console.log(media.toFixed(2))
+
+// Converter o valor númerico para String.
+// O método toString() retorna uma string representando o objeto Number especificado.
+console.log(media.toString())
+
+console.log(media.toString(2)) // converte em binário.
+
+console.log(typeof media)
+console.log(typeof Number)
