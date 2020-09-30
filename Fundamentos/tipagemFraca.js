ad66cd084dbcc11500964bd44fc59fb56d7eaea7
AristotelesPantaleao <62292967+AristotelesPantaleao@users.noreply.github.com>
2020-09-30 18:10:55 -0300

    Create tipagemFraca.js

---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,15 @@
+// Tipagem Fraca.
+
+// Tanto '' quandto "" podem ser usados para indicar String.
+let qualquer = 'Legal'
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//O operador typeof retorna uma string indicando o tipo de um operando.
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evitar nome genérico e siglas.
