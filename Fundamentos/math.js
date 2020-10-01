c2d8765462f005b6a5a1bafcccfc0c21140d9365
AristotelesPantaleao <62292967+AristotelesPantaleao@users.noreply.github.com>
2020-10-01 18:25:06 -0300

    Create math.js

---
+++ Fundamentos/math.js
@@ -0,0 +1,8 @@
+// Usando Math.
+
+const raio = 5.6
+// A propriedade Math.PI representa a proporção entre circunferência de um círculo com o seu diâmetro, aproximadamente 3.14159.
+// A função Math.pow() retorna a base elevada ao expoente power.
+const area = Math.PI * Math.pow(raio,2)
+console.log(area)
+console.log(typeof Math)
