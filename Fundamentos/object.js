7d6ce246ccd3f659d454a98582b6d27be8566e22
AristotelesPantaleao <62292967+AristotelesPantaleao@users.noreply.github.com>
2020-10-07 19:43:03 -0300

    Create object.js

---
+++ Fundamentos/object.js
@@ -0,0 +1,13 @@
+const prod1 = {} // Forma literal
+prod1.nome = 'Celular'
+prod1.preco = 4998.90
+console.log(prod1)
+
+prod1['Desconto'] =0.40 // Notação colchete
+console.log(prod1) // evita atributos com espaço
+
+const prod2 = {
+    nome:'Camisa Polo',
+    preco: 79.90
+}
+console.log(prod2)
