d8518397d5e7aa1a782222036eed0ae7017e760f
AristotelesPantaleao <62292967+AristotelesPantaleao@users.noreply.github.com>
2020-10-08 16:51:41 -0300

    Update funcaoVsObjeto.js

---
+++ Fundamentos/funcaoVsObjeto.js
@@ -0,0 +1,11 @@
+// O operador typeof retorna uma string indicando o tipo de um operando.
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const Cliente = function () {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {} // ES2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto())
