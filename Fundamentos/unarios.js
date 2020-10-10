fe2ccdd031b1dd46976c52a6159abc578597e792
AristotelesPantaleao <62292967+AristotelesPantaleao@users.noreply.github.com>
2020-10-10 13:12:44 -0300

    Create unarios.js

---
+++ Fundamentos/unarios.js
@@ -0,0 +1,13 @@
+let num1 = 1
+let num2 = 2
+
+num1++ // Acrescenta um valor a mais no variável
+
+console.log(num1)
+
+--num1
+console.log(num1)
+
+console.log(++num1 === num2--) // o -- só foi executado depois da comparação
+
+console.log(num1 === num2)
